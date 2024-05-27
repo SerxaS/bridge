8feb6f8cf6d9df7ed91706e2b2d77405bf1b0b86
serhas <serhas@gmail.com>
2024-05-27 16:57:11 +0300

    Made some changes.

--- src/gadgets/crypto.rs
+++ src/gadgets/crypto.rs
@@ -1,7 +1,45 @@
-use halo2curves::secp256k1::{Fq, Secp256k1};
+use halo2curves::ff::PrimeField;
+use halo2curves::{
+    group::{Curve, Group},
+    secp256k1::{Fp, Fq, Secp256k1},
+};
+
 use hex;
 use num_bigint::BigUint;
 use num_traits::Num;
+
+/// Converts Private Key to base10.
+pub fn key_to_base_ten(key: Fq) -> BigUint {
+    let mut temp = Vec::new();
+
+    let key_to_byte = key.to_bytes();
+
+    for i in key_to_byte {
+        temp.push(i.into())
+    }
+
+    temp.reverse();
+
+    let key_to_hex = hex::encode(temp);
+
+    let key_base_ten = BigUint::from_str_radix(&key_to_hex, 16)
+        .unwrap()
+        .to_string()
+        .parse()
+        .unwrap();
+    println!("priv_key_base10: {:?}", key_base_ten);
+
+    key_base_ten
+}
+
+/// Helper function to retrieves shared secret.
+pub fn gen_shared_secret(priv_key: Fq, pub_key_base_ten: BigUint) -> BigUint {
+    let priv_key_base_ten = key_to_base_ten(priv_key);
+
+    let shared = priv_key_base_ten * pub_key_base_ten;
+
+    shared
+}
 
 /// Helper function for compresses the Public Key's x & y coordinates into common format.
 fn compress(pub_key: Secp256k1) -> String {
@@ -24,29 +62,6 @@ fn compress(pub_key: Secp256k1) -> String {
     compressed_pub
 }
 
-/// Converts Private Key to base10. 
-pub fn key_to_base_ten(key: Fq) -> BigUint {
-    let mut temp_vec = Vec::new();
-
-    let key_to_byte = key.to_bytes();
-
-    for i in key_to_byte {
-        temp_vec.push(i.into())
-    }
-
-    temp_vec.reverse();
-
-    let key_to_hex = hex::encode(temp_vec);
-
-    let key_base_ten = BigUint::from_str_radix(&key_to_hex, 16)
-        .unwrap()
-        .to_string()
-        .parse()
-        .unwrap();
-    println!("priv key base10: {:?}", key_base_ten);
-    key_base_ten
-}
-
 /// Gets compressed Public Key from account object.
 pub fn get_compressed_pub_from_account(priv_key: Fq) -> String {
     ///// TODO will we need this? /////
@@ -56,21 +71,12 @@ pub fn get_compressed_pub_from_account(priv_key: Fq) -> String {
 
     //  Multiply private key by generator. Creates a Public Key.
     let pub_key = Secp256k1::generator() * priv_key;
-    println!("pub key: {:?}", pub_key);
+    println!("pub_key: {:?}", pub_key);
 
     // Compress Public Key.
     let compressed_pub_key = compress(pub_key);
 
     compressed_pub_key
-}
-
-/// Helper function to retrieves shared secret.
-pub fn gen_shared_secret(priv_key: Fq, pub_key_base_ten: BigUint) -> BigUint {
-    let priv_key_base_ten = key_to_base_ten(priv_key);
-
-    let shared = priv_key_base_ten * pub_key_base_ten;
-
-    shared
 }
 
 /// Gets y coordinate of compressed Public Key.
@@ -81,34 +87,28 @@ pub fn decompress_public_key(compressed_pub_key: String) -> BigUint {
 
     let b_hex = "0000000000000000000000000000000000000000000000000000000000000007";
     let b: BigUint = BigUint::from_str_radix(&b_hex, 16).unwrap();
-    println!("compressed pub key: {:?}", compressed_pub_key);
+    println!("compressed_pub_key_x: {:?}", compressed_pub_key);
+
     let mut x_hex = compressed_pub_key.chars();
     x_hex.next();
     x_hex.next();
-    println!("{:?}", x_hex.as_str());
+    println!("decompressed_x_hex: {:?}", x_hex.as_str());
 
     let x: BigUint = BigUint::from_str_radix(&x_hex.as_str(), 16).unwrap();
-    println!("{:?}", x);
+    println!("decompressed_x_dec: {:?}", x);
 
-    /// TODO fix parity!!!
+    // TODO fix parity!!!
     // Secp256k1 is chosen in a special way so that the square root of y is y^((p+1)/4).
-    let y_square = (BigUint::pow(&x, 3) + b) % p.clone();
-    let y = BigUint::modpow(&y_square, &((p.clone() + 1usize) / 4usize), &p);
+    //let y_square = (BigUint::modpow(&x, &BigUint::from(3usize), &p) + b) % &p;
+    let y_square = BigUint::pow(&x, 3u32) + b;
+    let y = BigUint::modpow(&y_square, &((&p + 1usize) / 4usize), &p);
 
-    println!("{:?}", y);
+    let y_hex = BigUint::to_str_radix(&y, 16);
 
-    y
-    // let y_str = y.to_str_radix(16);
-    // let mut y_str_byte = y_str.as_bytes().to_vec();
-    // println!("{:?}", y_str_byte);
+    println!("y_hex: {:?}", y_hex);
+    println!("y: {:?}", y);
 
-    // y_str_byte.reverse();
-
-    // let y_arr: [u8; 64] = y_str_byte.try_into().unwrap_or_else(|v: Vec<u8>| {
-    //     panic!("Expected a Vec of length {} but it was {}", 64, v.len())
-    // });
-
-    // let y_str_fr = Fq::from_uniform_bytes(&y_arr);
+    p
 }
 
 /// TODO Check difference between python function!
@@ -116,6 +116,6 @@ pub fn decompress_public_key(compressed_pub_key: String) -> BigUint {
 pub fn public_key_from_raw_priv(raw_priv: Fq) -> String {
     let pub_key = Secp256k1::generator() * raw_priv;
     let pub_key_compressed = compress(pub_key);
-    println!("{:?}", pub_key_compressed);
+    println!("pub_raw: {:?}", pub_key_compressed);
     pub_key_compressed
 }
