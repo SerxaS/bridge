5083be45354b04a67de349ab057c056809d3e075
serhas <serhas@gmail.com>
2024-06-25 09:44:08 +0300

    Account file has been changed.

--- src/middleware/account.rs
+++ src/middleware/account.rs
@@ -4,7 +4,6 @@ use alloy::hex::ToHexExt;
 use alloy::primitives::Uint;
 use alloy::signers::wallet::LocalWallet;
 use num_bigint::BigUint;
-use num_traits::Num;
 
 /// TODO: Check prints in get_shared_secret!
 /// Wrapper for managing a given web3 account.
@@ -47,14 +46,14 @@ impl SemaphoreNetworkAccount {
     fn get_shared_secret(&self, subscriber_index: Uint<256, 4>) -> BigUint {
         println!("Subscriber SUID index to lookup on chain (RAW) {subscriber_index}");
 
-        // Query web3 rpc @subscriber index, cast to int.
+        // Queries web3 rpc @subscriber index, cast to int.
         let subscriber_pub = self.transactions.get_subscriber_pub_key(subscriber_index);
         println!("Subscriber @{subscriber_index}'s (compressed) pubkey is {subscriber_pub}");
 
         let subscriber_pub_hex = subscriber_pub.encode_hex();
         println!("Subscribers network ID (SNID): {subscriber_pub_hex}");
 
-        // Generate shared secret.
+        // Generates shared secret.
         let shared_secret = self.crypto.gen_shared_secret(
             self.web3_account
                 .clone()
@@ -64,4 +63,28 @@ impl SemaphoreNetworkAccount {
 
         shared_secret
     }
+
+    /// TODO: fn() that imports json wallet account & decrypts w/ env(?) password? What does it mean?
+    fn import_account() {}
+
+    /// TODO: It doesnt take subscriber idx! Check! PK must be private key?
+    /// Takes a subscriber index and converts to a subscriber web3 account."
+    fn account_from_pk() {}
+
+    /// TODO: We have same fn in Crypro!
+    /// Derives the uncompressed public key, either for a given web3 account or for, by
+    /// default, the nested web3 account we have for the provider here.
+    fn get_uncompressed_pub_from_account() {}
+
+    // TODO: If they are just for shows the data, i can do them in transaction!
+    // CONTRACT CALLS
+
+    /// Calls `getSubscriberKey` in the SemaphoreHSS contract.
+    fn hss_get_subscriber_pubkey_from_chain() {}
+
+    /// TODO: Same as above!
+    fn payments_check_balance() {}
+
+    /// TODO: Same as above!
+    fn payments_claim() {}
 }
